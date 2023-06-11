16235dde8eeba1777714d939df0c7c93962bfd81
Leander Seige <leander@seige.name>
2023-06-11 13:45:07 +0200

    update

--- src/utils/warmup.js
+++ src/utils/warmup.js
@@ -1,13 +1,16 @@
 const fetch = require('cross-fetch')
 const config = require('../config.json')
 
+console.log("Fetching list of datasets ...")
 fetch('https://opendata.leipzig.de/api/3/action/dataset_search?fq=res_format:jpg&rows=1000').then(
   (response) => response.json()
 ).then( async (data) => {
+  console.log("Start Processing.\nFinished datasets:")
   for(x in data.result.results) {
     // let murl = config.iiifBaseUri+"/manifest/"+data.result.results[x].id
     let murl = "http://localhost:2000/manifest/"+data.result.results[x].id
-    console.log("Processing "+murl)
     await fetch(murl)
+    console.log(murl)
   }
+  console.log("Done.")
 })
